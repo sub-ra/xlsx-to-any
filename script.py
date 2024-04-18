0441173ab103709a178cb67d8adcbd1e4f36bdef
sub-ra <87712870+sub-ra@users.noreply.github.com>
2024-04-18 16:45:04 +0200

    Update script.py
    Added "pipe", "yaml", "xml", "html", "csv", "json", "plain", "jira", "mediawiki" as available Formats

--- script.py
+++ script.py
@@ -3,20 +3,20 @@ import os
 import openpyxl
 import tkinter as tk
 import warnings
-from tkinter import filedialog, messagebox
+from tkinter import filedialog, messagebox, ttk
 from tabulate import tabulate
 
-# Filter UserWarning in Console
+# UserWarning ausblenden 
 warnings.simplefilter(action='ignore', category=UserWarning)
 
 # Initialize Tkinter
 root = tk.Tk()
 root.title("Excel-to-Mardown-Converter")
 
-# Exit Script on Window Close
+# Script beenden wenn Fenster geschlossen wird
 root.bind("<Destroy>", lambda e: sys.exit())
 
-# Function to open File Dialog
+# Funktion zum Öffnen des Dateiauswahldialogs
 def open_file_dialog():
     file_path = filedialog.askopenfilename(
         parent=root,
@@ -26,31 +26,38 @@ def open_file_dialog():
     if file_path:
         process_workbook(file_path)
 
-#Function to process selected XLSX File
+# Funktion zum Verarbeiten der ausgewählten Excel-Datei
 def process_workbook(file_path):
     try:
-        # Load XLSX with openpyxl
+        # Excel-Datei mit openpyxl laden
         workbook = openpyxl.load_workbook(file_path, data_only=True)
     except Exception as e:
         messagebox.showinfo("Fehler", f"Fehler beim Laden der Excel-Datei: {str(e)}")
         return
+    
+    # List of available formats
+    formats = ["pipe", "yaml", "xml", "html", "csv", "json", "plain", "jira", "mediawiki"]
 
-    # Create Label
+    # Create a StringVar for the combobox
+    selected_format = tk.StringVar()
+
+
+    # Label erstellen
     tk.Label(root, text= "Excel-Arbeitsblätter zum Exportieren auswählen:").pack(anchor='w')
 
-    # List Sheets
+    # Liste der verfügbaren Arbeitsblätter
     available_sheets = workbook.sheetnames
 
-    # Errorhandling - Check if Sheets are available
+    # Check ob Arbeitsmappen vorhanden sind
     if not available_sheets:
         messagebox.showinfo("Fehler", "Die ausgewählte Excel-Datei enthält keine Arbeitsblätter.")
         return
 
-    # Listbox for selecting Sheets
+    # Listbox für die Auswahl der Arbeitsblätter
     listbox = tk.Listbox(root, selectmode='multiple', exportselection=0)
     listbox.pack(side='top', fill='both', expand=True)
 
-    # Add Sheets to Listbox
+    # Arbeitsblätter zur Listbox hinzufügen
     for sheet in available_sheets:
         listbox.insert(tk.END, sheet)
 
@@ -60,7 +67,16 @@ def process_workbook(file_path):
     # Create Checkbox
     tk.Checkbutton(root, text="Ausgeblendete Zeilen/Spalten beim Export nicht einbeziehen", variable=export_visible).pack(anchor='w')
 
-    # Functions for Buttons
+    # Label for the combobox
+    tk.Label(root, text="Wähle gewünschtes Format:").pack(anchor='w')
+
+    # Create the combobox
+    combobox = ttk.Combobox(root, textvariable=selected_format, state='readonly')
+    combobox['values'] = formats
+    combobox.current(0)  # set initial selection
+    combobox.pack(anchor='w')
+
+    # Funktionen für die Buttons
     def select_all():
         listbox.select_set(0, tk.END)
 
@@ -76,7 +92,7 @@ def process_workbook(file_path):
         selected_indices = listbox.curselection()
         selected_sheets = [listbox.get(i) for i in selected_indices]
 
-        # Errorhandling - Check if Sheets selected
+        # Check ob Arbeitsmappen ausgewählt wurden
         if not selected_sheets:
             messagebox.showinfo("Information", "Bitte wähle Arbeitsmappen aus")
             return
@@ -89,41 +105,47 @@ def process_workbook(file_path):
                 data = []
 
                 for row in sheet.iter_rows(min_row=1, max_col=sheet.max_column, max_row=sheet.max_row):
-                    # Check Column Visibility - if Checkbox active
+                    # Prüft ob Spalte versteckt ist - wenn Checkbox aktiviert
                     if export_visible.get() and sheet.row_dimensions[row[0].row].hidden:
                         continue
                     row_data = []
                     for cell in row:
-                        # Check Row Visibility - if Checkbox active
+                        # Prüft ob Zeile versteckt ist - wenn Checkbox aktiviert
                         column_dimension = sheet.column_dimensions.get(cell.column_letter)
                         if not export_visible.get() or (column_dimension is not None and not column_dimension.hidden):
                             row_data.append(cell.value)
-                    # If Colum not empty -  transfer to Data
+                    # Wenn Spalte nicht leer, in Data übertragen
                     if row_data:
                         data.append(row_data)
 
-                # Convert Data to Markdown Table
-                markdown_table = tabulate(data, tablefmt="pipe", headers="firstrow", showindex=False)
+                # Daten in gewähltes Format konvertieren
+                format_table = tabulate(data, tablefmt=selected_format.get(), headers="firstrow", showindex=False)
+ 
+                # Dictionary mapping formats to file extensions
+                format_extensions = {"pipe": ".md", "plain": ".txt", "jira": ".txt", "mediawiki": ".txt", "csv": ".csv", "json": ".json", "yaml": ".yaml", "xml": ".xml", "html": ".html"}
 
-                # Set Path for Markdown-File
-                output_file_path = f"{file_path.rsplit('.', 1)[0]}_{sheet_name}.md"
+                # Get the file extension for the selected format
+                file_extension = format_extensions[selected_format.get()]
 
-                # Errorhandling - Check if Filename exists
+                # Pfad für die Markdown-Datei setzen
+                output_file_path = f"{file_path.rsplit('.', 1)[0]}_{sheet_name}{file_extension}"
+
+                # Check ob Dateiname bereits existiert
                 if os.path.isfile(output_file_path):
                     overwrite = messagebox.askyesno("Datei existiert bereits", f"Die Datei {output_file_path} existiert bereits. Überschreiben?")
                     if not overwrite:
                         return
 
-                # Write Markdown-Table to File
+                # Markdown-Tabelle in eine Datei schreiben
                 try:
                     with open(output_file_path, "w", encoding="utf-8") as f:
-                        f.write(markdown_table)
+                        f.write(format_table)
                 except Exception as e:
                     messagebox.showinfo("Fehler", f"Fehler beim Schreiben der Datei {output_file_path}: {str(e)}")
                     return
 
-        # Success Message
-        messagebox.showinfo("Erfolg", "MD-File(s) erfolgreich erstellt.")
+        # Erfolgsmeldung anzeigen
+        messagebox.showinfo("Erfolg", "File(s) erfolgreich erstellt.")
         root.quit()
 
     # Buttons
@@ -139,8 +161,8 @@ def process_workbook(file_path):
     button_cancel = tk.Button(root, text="Abbrechen", command=cancel)
     button_cancel.pack(side='left', fill='x', expand=True)
 
-# Open File Dialog
+# Öffne den Dateiauswahldialog
 open_file_dialog()
 
-# Start Tkinter Event-Loop
+# Tkinter Event-Schleife starten
 root.mainloop()
