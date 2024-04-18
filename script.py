74f9b12962baf2b0846d84a9546b0066acf21902
sub-ra <87712870+sub-ra@users.noreply.github.com>
2024-04-18 17:55:07 +0200

    Update script.py
    Translation from partial german to fully english

--- script.py
+++ script.py
@@ -14,33 +14,33 @@ from tabulate import tabulate
 import yaml
 import pandas as pd
 
-# UserWarning ausblenden 
+# Hide UserWarning
 warnings.simplefilter(action='ignore', category=UserWarning)
 
 # Initialize Tkinter
 root = tk.Tk()
 root.title("Excel-to-Mardown-Converter")
 
-# Script beenden wenn Fenster geschlossen wird
+# Terminate the script when the window is closed
 root.bind("<Destroy>", lambda e: sys.exit())
 
-# Funktion zum Öffnen des Dateiauswahldialogs
+# Function to open the file selection dialog
 def open_file_dialog():
     file_path = filedialog.askopenfilename(
         parent=root,
-        title="Bitte wählen Sie das Excel-Dokument aus.",
+        title="Please select the Excel document.",
         filetypes=[("Excel Files", "*.xlsx"), ("All Files", "*.*")]
     )
     if file_path:
         process_workbook(file_path)
 
-# Funktion zum Verarbeiten der ausgewählten Excel-Datei
+# Function to process the selected Excel file
 def process_workbook(file_path):
     try:
-        # Excel-Datei mit openpyxl laden
+        # Load Excel file with openpyxl
         workbook = openpyxl.load_workbook(file_path, data_only=True)
     except Exception as e:
-        messagebox.showinfo("Fehler", f"Fehler beim Laden der Excel-Datei: {str(e)}")
+        messagebox.showinfo("Error", f"Error loading the Excel file: {str(e)}")
         return
     
     # List of available formats
@@ -49,23 +49,22 @@ def process_workbook(file_path):
     # Create a StringVar for the combobox
     selected_format = tk.StringVar()
 
+    # Create a label
+    tk.Label(root, text= "Select Excel worksheets to export:").pack(anchor='w')
 
-    # Label erstellen
-    tk.Label(root, text= "Excel-Arbeitsblätter zum Exportieren auswählen:").pack(anchor='w')
-
-    # Liste der verfügbaren Arbeitsblätter
+    # List of available worksheets
     available_sheets = workbook.sheetnames
 
-    # Check ob Arbeitsmappen vorhanden sind
+    # Check if worksheets are available
     if not available_sheets:
-        messagebox.showinfo("Fehler", "Die ausgewählte Excel-Datei enthält keine Arbeitsblätter.")
+        messagebox.showinfo("Error", "The selected Excel file does not contain any worksheets.")
         return
 
-    # Listbox für die Auswahl der Arbeitsblätter
+    # Listbox for selecting worksheets
     listbox = tk.Listbox(root, selectmode='multiple', exportselection=0)
     listbox.pack(side='top', fill='both', expand=True)
 
-    # Arbeitsblätter zur Listbox hinzufügen
+    # Add worksheets to the listbox
     for sheet in available_sheets:
         listbox.insert(tk.END, sheet)
 
@@ -73,10 +72,10 @@ def process_workbook(file_path):
     export_visible = tk.BooleanVar(value=True)
 
     # Create Checkbox
-    tk.Checkbutton(root, text="Ausgeblendete Zeilen/Spalten beim Export nicht einbeziehen", variable=export_visible).pack(anchor='w')
+    tk.Checkbutton(root, text="Exclude hidden rows/columns when exporting", variable=export_visible).pack(anchor='w')
 
     # Label for the combobox
-    tk.Label(root, text="Wähle gewünschtes Format:").pack(anchor='w')
+    tk.Label(root, text="Choose desired format:").pack(anchor='w')
 
     # Create the combobox
     combobox = ttk.Combobox(root, textvariable=selected_format, state='readonly')
@@ -84,7 +83,7 @@ def process_workbook(file_path):
     combobox.current(0)  # set initial selection
     combobox.pack(anchor='w')
 
-    # Funktionen für die Buttons
+    # Functions for the buttons
     def select_all():
         listbox.select_set(0, tk.END)
 
@@ -100,9 +99,9 @@ def process_workbook(file_path):
         selected_indices = listbox.curselection()
         selected_sheets = [listbox.get(i) for i in selected_indices]
 
-        # Check ob Arbeitsmappen ausgewählt wurden
+        # Check if worksheets were selected
         if not selected_sheets:
-            messagebox.showinfo("Information", "Bitte wähle Arbeitsmappen aus")
+            messagebox.showinfo("Information", "Please select worksheets")
             return
 
         for sheet_name in selected_sheets:
@@ -113,64 +112,64 @@ def process_workbook(file_path):
                 data = []
 
                 for row in sheet.iter_rows(min_row=1, max_col=sheet.max_column, max_row=sheet.max_row):
-                    # Prüft ob Spalte versteckt ist - wenn Checkbox aktiviert
+                    # Check if column is hidden - if checkbox is active
                     if export_visible.get() and sheet.row_dimensions[row[0].row].hidden:
                         continue
                     row_data = []
                     for cell in row:
-                        # Prüft ob Zeile versteckt ist - wenn Checkbox aktiviert
+                        # Check if row is hidden - if checkbox is active
                         column_dimension = sheet.column_dimensions.get(cell.column_letter)
                         if not export_visible.get() or (column_dimension is not None and not column_dimension.hidden):
                             row_data.append(cell.value)
-                    # Wenn Spalte nicht leer, in Data übertragen
+                    # If row is not empty, transfer to data
                     if row_data:
                         data.append(row_data)
 
-                # Daten in gewähltes Format konvertieren
+                # Convert data to chosen format
                 format_table = tabulate(data, tablefmt=selected_format.get(), headers="firstrow", showindex=False)
- 
+
                 # Dictionary mapping formats to file extensions
                 format_extensions = {"pipe": ".md", "plain": ".txt", "jira": ".txt", "mediawiki": ".txt", "csv": ".csv", "json": ".json", "yaml": ".yaml", "xml": ".xml", "html": ".html"}
 
                 # Get the file extension for the selected format
                 file_extension = format_extensions[selected_format.get()]
 
-                # Pfad für die Markdown-Datei setzen
+                # Path for the Markdown file
                 output_file_path = f"{file_path.rsplit('.', 1)[0]}_{sheet_name}{file_extension}"
 
-                # Check ob Dateiname bereits existiert
+                # Check if filename already exists
                 if os.path.isfile(output_file_path):
-                    overwrite = messagebox.askyesno("Datei existiert bereits", f"Die Datei {output_file_path} existiert bereits. Überschreiben?")
+                    overwrite = messagebox.askyesno("File already exists", f"The file {output_file_path} already exists. Overwrite?")
                     if not overwrite:
                         return
 
-                # Markdown-Tabelle in eine Datei schreiben
+                # Write the Markdown table into a file
                 try:
                     with open(output_file_path, "w", encoding="utf-8") as f:
                         f.write(format_table)
                 except Exception as e:
-                    messagebox.showinfo("Fehler", f"Fehler beim Schreiben der Datei {output_file_path}: {str(e)}")
+                    messagebox.showinfo("Error", f"Error writing the file {output_file_path}: {str(e)}")
                     return
 
-        # Erfolgsmeldung anzeigen
-        messagebox.showinfo("Erfolg", "File(s) erfolgreich erstellt.")
+        # Show success message
+        messagebox.showinfo("Success", "File(s) successfully created.")
         root.quit()
 
     # Buttons
-    button_select_all = tk.Button(root, text="Alle auswählen", command=select_all)
+    button_select_all = tk.Button(root, text="Select all", command=select_all)
     button_select_all.pack(side='left', fill='x', expand=True)
 
-    button_deselect_all = tk.Button(root, text="Auswahl aufheben", command=deselect_all)
+    button_deselect_all = tk.Button(root, text="Deselect all", command=deselect_all)
     button_deselect_all.pack(side='left', fill='x', expand=True)
 
-    button_export = tk.Button(root, text="Exportieren", command=export_sheets)
+    button_export = tk.Button(root, text="Export", command=export_sheets)
     button_export.pack(side='left', fill='x', expand=True)
 
-    button_cancel = tk.Button(root, text="Abbrechen", command=cancel)
+    button_cancel = tk.Button(root, text="Cancel", command=cancel)
     button_cancel.pack(side='left', fill='x', expand=True)
 
-# Öffne den Dateiauswahldialog
+# Open the file selection dialog
 open_file_dialog()
 
-# Tkinter Event-Schleife starten
+# Start Tkinter event loop
 root.mainloop()
